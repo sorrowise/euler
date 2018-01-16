04efd8b84aa53cc099aeddd016876d282b81cb6c
metaquant <576422965@qq.com>
2018-01-16 10:15:25 +0800

    Update 045.py

--- 045.py
+++ 045.py
@@ -2,10 +2,6 @@ t = lambda n : n*(n+1)/2
 p = lambda n : n*(3*n-1)/2
 h = lambda n : n*(2*n-1)
 
-ts = set([t(n) for n in range(285,301)])
-ps = set([p(n) for n in range(165,301)])
-hs = set([h(n) for n in range(143,301)])
-
 up = 301
 while True:
     ts = set([t(n) for n in range(286,up)])
