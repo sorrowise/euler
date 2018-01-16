7c5f37b28cc1869a1fc1ecb2239db904489c487a
metaquant <576422965@qq.com>
2018-01-16 10:20:56 +0800

    Update 045.py

--- 045.py
+++ 045.py
@@ -2,7 +2,7 @@ t = lambda n : n*(n+1)/2
 p = lambda n : n*(3*n-1)/2
 h = lambda n : n*(2*n-1)
 
-up = 301
+up = 10001
 while True:
     ts = set([t(n) for n in range(286,up)])
     ps = set([p(n) for n in range(166,up)])
