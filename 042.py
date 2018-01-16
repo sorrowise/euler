061982c84cd70cbf974ed86d1f2b902fa0fbf547
metaquant <576422965@qq.com>
2018-01-16 15:08:11 +0800

    Create 042.py

---
+++ 042.py
@@ -0,0 +1,15 @@
+with open('euler/ep42.txt','r') as f:
+    data = f.read().replace('"',"").split(',')
+
+def alphabet(word):
+    alpha = lambda s : ord(s) - ord('A') + 1
+    res = sum([alpha(x) for x in word])
+    return res
+
+word_value = [alphabet(x) for x in data]
+triangle_num = [n*(n+1)//2 for n in range(1,25)]
+num = 0
+for x in word_value:
+    if x in triangle_num:
+        num += 1
+print(num)
