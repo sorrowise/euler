d6b389546477b55ece408cfac807d5e0a36ab149
metaquant <576422965@qq.com>
2018-01-17 20:43:43 +0800

    Create 032.py

---
+++ 032.py
@@ -0,0 +1,15 @@
+def is_pandgit(x):
+    pandgit_set = {int(x) for x in str(x)}
+    if pandgit_set == set(range(1,len(str(x))+1)):
+        return True
+    return False
+
+res = set()
+for a in range(1,100):
+    start = 1234 if a <=9 else 123
+    for b in range(start,10000//a):
+        prod = a * b
+        total_str = int(str(a) + str(b) + str(prod))
+        if is_pandgit(total_str):
+            res.add(prod)
+print(sum(res))
