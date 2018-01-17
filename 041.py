648fbfba749467db62329424319858b1079ab0cc
metaquant <576422965@qq.com>
2018-01-17 16:58:29 +0800

    Update 041.py

--- 041.py
+++ 041.py
@@ -17,7 +17,7 @@ from operator import add
 from itertools import permutations
 from sympy import sieve
 
-def main2():
+def main():
     number_list = [str(x) for x in range(1,8)]
     res_list = [int(reduce(add,x)) for x in list(permutations(number_list,7))]
     cross_set = set(res_list) & set(sieve.primerange(1234567,7654321))
