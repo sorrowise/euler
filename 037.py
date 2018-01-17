26459fa65110829f21a707970a4b39318f25d678
metaquant <576422965@qq.com>
2018-01-17 15:37:50 +0800

    Update 037.py

--- 037.py
+++ 037.py
@@ -3,7 +3,7 @@ from sympy import sieve
 def is_truncprime(x,lst):
     s = str(x)
     for d in range(1,len(s)):
-        if int(s[d:]) not in lst or int(s[:d]) not in lst:
+        if int(s[d:]) not in primes_filter or int(s[:d]) not in lst:
             return False
     return True
 
@@ -16,4 +16,4 @@ def sum_of_truncprimes():
         primes_filter = [x for x in primes if set(str(x))-odd_set == set() or x<100]
         res = [x for x in primes_filter if is_truncprime(x,primes_filter)]
         upbound += 10000
-        return sum(res) - sum([2,3,5,7])
+    return sum(res) - sum([2,3,5,7])
