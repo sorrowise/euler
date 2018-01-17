9622879ecbd789d31d59f75e23f3e145eeeeb826
metaquant <576422965@qq.com>
2018-01-17 16:57:51 +0800

    Update 041.py

--- 041.py
+++ 041.py
@@ -1,3 +1,5 @@
+# approach 1, time cost = 1.95s
+
 from sympy import sieve
 
 def is_pandgit(x):
@@ -7,3 +9,16 @@ def is_pandgit(x):
     return False
 
 max([x for x in list(sieve.primerange(1,7654321)) if is_pandgit(x)])
+
+# approach 2, time cost = 180ms
+
+from functools import reduce
+from operator import add
+from itertools import permutations
+from sympy import sieve
+
+def main2():
+    number_list = [str(x) for x in range(1,8)]
+    res_list = [int(reduce(add,x)) for x in list(permutations(number_list,7))]
+    cross_set = set(res_list) & set(sieve.primerange(1234567,7654321))
+    return max(cross_set)
