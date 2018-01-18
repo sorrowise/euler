031364fb8b1afb27cae74047eb59c01b057a2774
metaquant <576422965@qq.com>
2018-01-18 16:51:31 +0800

    Update 067.py

--- 067.py
+++ 067.py
@@ -1,8 +1,10 @@
+# time cost = 3.18ms
+
 with open('euler/ep67.txt') as f:
     table = [list(map(int,s.split())) for s in f.readlines()]
 
-for row in range(len(table)-1, 0, -1):
-    for col in range(0, row):
-        table[row-1][col] += max(table[row][col], table[row][col+1])
-
-print(table[0][0])
+def main():
+    for row in range(len(table)-1, 0, -1):
+        for col in range(0, row):
+            table[row-1][col] += max(table[row][col], table[row][col+1])
+    return table[0][0]
