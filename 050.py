f025d27bb5a72aa0d69d8e1ce9ed22164813b408
metaquant <576422965@qq.com>
2018-01-18 11:24:40 +0800

    Update 050.py

--- 050.py
+++ 050.py
@@ -3,9 +3,10 @@
 from sympy import sieve
 from sympy.ntheory import isprime
 
-def main():
-    primes = list(sieve.primerange(1,3943))
+def main(upbound=3943):
+    primes = list(reversed(list(sieve.primerange(1,upbound))))
     for d in range(len(primes)-1,0,-1):
         for start in range((len(primes)-d+1)):
-            if isprime(sum(primes[start:start+d])):
-                return sum(primes[start:start+d])
+            res = sum(primes[start:start+d])
+            if isprime(res):
+                return res
