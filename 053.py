55d150dff89951586eb5dcfe0f9138b951751b79
metaquant <576422965@qq.com>
2018-01-19 09:34:00 +0800

    Create 053.py

---
+++ 053.py
@@ -0,0 +1,15 @@
+# time cost = 12.6ms
+
+from math import factorial as fac
+
+def comb_num(n,k):
+    num = fac(n)/(fac(n-k)*fac(k))
+    return num
+
+def main():
+    num = 0
+    for n in range(1,101):
+        for r in range(2,n):
+            if comb_num(n,r) > 1e6:
+                num += 1
+    return num
