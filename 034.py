45d47a15540631b8653027e715275084d4c24946
metaquant <576422965@qq.com>
2019-10-05 15:43:58 +0800

    Update 034.py

--- 034.py
+++ 034.py
@@ -1,7 +1,11 @@
+# time cost = 2.22 s ± 3.31 ms
+
 from math import factorial
-fac_list = [factorial(n) for n in range(10)]
-res = 0
-for i in range(10,2177282):
-    if sum([fac_list[int(x)] for x in str(i)]) == i:
-        res += i
-print(res)
+
+def main():
+    fac_dict = {str(n):factorial(n) for n in range(10)}
+    res = 0
+    for i in range(10,2177282):
+        if sum([fac_dict[x] for x in str(i)]) == i:
+            res += i
+    return res
