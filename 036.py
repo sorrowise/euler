6bff1d971ca6c5ea17fc6dac3b410fec9ef35118
metaquant <576422965@qq.com>
2019-10-08 10:31:38 +0800

    Update 036.py

--- 036.py
+++ 036.py
@@ -1,6 +1,19 @@
-# ans = 872187, time cost = 878ms
+# time cost = 2.5 ms ± 13 µs
+
+def make_palindrome(x):
+    s = str(x)
+    p1 = s + s[::-1]
+    p2 = s + s[:-1][::-1]
+    return int(p1),int(p2)
 
 def main():
-    is_palindrome = lambda x : str(x) == str(x)[::-1]
-    res = sum([x for x in range(1,10**6) if is_palindrome(x) and is_palindrome(int(bin(x)[2:]))])
+    is_palindrome = lambda x : x[-1]!=0 and x == x[::-1]
+    res = 0
+    for i in range(1,1000):
+        p1,p2 = make_palindrome(i)
+        p1_base2,p2_base2 = bin(p1)[2:],bin(p2)[2:]
+        if is_palindrome(p1_base2):
+            res = res + p1
+        if is_palindrome(p2_base2):
+            res = res + p2
     return res
