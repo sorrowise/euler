1aedb353966353954f5d15b73654f60ece5d4ab0
metaquant <576422965@qq.com>
2019-10-09 15:09:41 +0800

    Update 038.py

--- 038.py
+++ 038.py
@@ -1,14 +1,13 @@
-# ans = 932718654, time cost = 968 µs
-# see link: http://www.mathblog.dk/project-euler-38-pandigital-multiplying-fixed-number/
+# time cost = 360 µs ± 3.8 µs
 
-def is_pandgit(x):
-    pandgit_set = {int(x) for x in str(x)}
-    if pandgit_set == set(range(1,len(str(x))+1)):
+def is_pandgital(x):
+    digits_set = set(str(x))
+    if digits_set == set(str(123456789)):
         return True
     return False
 
 def main():
     for p in range(9487,9233,-1):
-        con = int(str(p) + str(p*2))
-        if is_pandgit(con):
-            return con
+        ans = int(str(p) + str(p*2))
+        if is_pandgital(ans):
+            return ans
