b6e403ee24614507e275143ecaab053cf61d94d5
metaquant <576422965@qq.com>
2019-10-11 09:55:26 +0800

    Update 039.py

--- 039.py
+++ 039.py
@@ -1,11 +1,13 @@
-max_p = 2
-max_num = 0
-for p in range(2,1001,2):
+# time cost = 60.1 ms ± 626 µs
+
+def number_of_solution(p):
     num = 0
     for a in range(1,p//3):
         if (p**2-2*p*a)%(2*p-2*a) == 0:
             num += 1
-    if num > max_num:
-        max_p = p
-        max_num = num
-print(max_p)
+    return num
+
+def main(n=1000):
+    d = {p:number_of_solution(p) for p in range(2,n+1,2)}
+    ans = max(d,key=d.get)
+    return ans
