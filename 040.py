513b865f398d32241a3009f8342644e663f63a95
metaquant <576422965@qq.com>
2019-10-11 17:25:24 +0800

    Update 040.py

--- 040.py
+++ 040.py
@@ -1,10 +1,26 @@
-# time cost = 3.96s
+# time cost =  69 µs ± 1 µs
 
-from functools import reduce
-from operator import add,mul
+def kd(n):
+    ub = lambda n : 9*sum([x*10**(x-1) for x in range(1,n+1)])
+    i = 1
+    while ub(i)<n:
+        i += 1
+    return i,ub(i-1)
+
+def d(n):
+    if n == 1:
+        return 1
+    elif n > 1:
+        k,u = kd(n)
+        m = (n-u) // k
+        r = (n-u) % k
+        t = 10**(k-1) + m + 1
+        dn = int(str(t)[r-1])
+        return dn
 
 def main():
-    res_str = reduce(add,[str(x) for x in range(1,190000)])
-    power = [10**x for x in range(7)]
-    return reduce(mul,[int(res_str[i-1]) for i in power])
-
+    n = [10**x for x in range(7)]
+    ans = 1
+    for i in n:
+        ans *= d(i)
+    return ans
