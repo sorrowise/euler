31c022ce7d675f7889a135922f6c99286e8aac7b
metaquant <576422965@qq.com>
2019-10-12 08:51:59 +0800

    Update 040.py

--- 040.py
+++ 040.py
@@ -1,19 +1,19 @@
 # time cost =  69 µs ± 1 µs
 
-def kd(n):
-    ub = lambda n : 9*sum([x*10**(x-1) for x in range(1,n+1)])
+def get_k_lb(n):
+    lower_bound = lambda n : 9*sum([x*10**(x-1) for x in range(1,n+1)])
     i = 1
-    while ub(i)<n:
+    while lower_bound(i)<n:
         i += 1
-    return i,ub(i-1)
+    return i,lower_bound(i-1)
 
-def d(n):
+def get_digit(n):
     if n == 1:
         return 1
     elif n > 1:
-        k,u = kd(n)
-        m = (n-u) // k
-        r = (n-u) % k
+        k,lb = get_k_lb(n)
+        m = (n-lb) // k
+        r = (n-lb) % k
         t = 10**(k-1) + m + 1
         dn = int(str(t)[r-1])
         return dn
@@ -22,5 +22,5 @@ def main():
     n = [10**x for x in range(7)]
     ans = 1
     for i in n:
-        ans *= d(i)
+        ans *= get_digit(i)
     return ans
