7cfe14387806166e0c14601886ce7282f5532da0
metaquant <576422965@qq.com>
2019-10-13 12:00:05 +0800

    Update 041.py

--- 041.py
+++ 041.py
@@ -1,24 +1,12 @@
-# approach 1, time cost = 1.95s
+# time cost = 1.92 ms ± 111 µs
 
-from sympy import sieve
-
-def is_pandgit(x):
-    pandgit_set = {int(x) for x in str(x)}
-    if pandgit_set == set(range(1,len(str(x))+1)):
-        return True
-    return False
-
-max([x for x in list(sieve.primerange(1234567,7654321)) if is_pandgit(x)])
-
-# approach 2, time cost = 180ms
-
-from functools import reduce
-from operator import add
-from itertools import permutations
-from sympy import sieve
+from sympy import prevprime
 
 def main():
-    number_list = [str(x) for x in range(1,8)]
-    res_list = [int(reduce(add,x)) for x in list(permutations(number_list,7))]
-    cross_set = set(res_list) & set(sieve.primerange(1234567,7654321))
-    return max(cross_set)
+    start = 7654321
+    while True:
+        prevp = prevprime(start)
+        if set(str(prevp)) == set('1234567'):
+            return prevp
+        else:
+            start = prevprime(prevp)  
