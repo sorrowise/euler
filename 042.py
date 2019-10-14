8387bbef54836d9bc50970750f87b6e7a2c0263e
metaquant <576422965@qq.com>
2019-10-14 09:06:41 +0800

    Update 042.py

--- 042.py
+++ 042.py
@@ -1,11 +1,15 @@
-with open('euler/ep42.txt','r') as f:
-    data = f.read().replace('"',"").split(',')
+# time cost = 5.08 ms ± 8.23 µs
+
+from math import sqrt
 
 def alphabet(word):
     alpha = lambda s : ord(s) - ord('A') + 1
     res = sum([alpha(x) for x in word])
     return res
 
-word_value = [alphabet(x) for x in data]
-triangle_num = [n*(n+1)//2 for n in range(1,25)]
-len([x for x in word_value if x in triangle_num])
+def main():
+    with open('euler/ep42.txt','r') as f:
+        data = f.read().replace('"',"").split(',')
+    word_value = [alphabet(x) for x in data]
+    res = [x for x in word_value if sqrt(1+8*x)%2==1]
+    return len(res)
