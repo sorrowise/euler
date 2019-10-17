8ffb1cb4a5fbd371d2e0859d2aa78883fd956a33
metaquant <576422965@qq.com>
2019-10-17 12:24:56 +0800

    Update 046.py

--- 046.py
+++ 046.py
@@ -1,19 +1,18 @@
-# time cost = 711ms
+# time cost = 19.7 ms ± 199 µs
 
 from math import sqrt
-from sympy.ntheory import isprime
+from sympy import isprime
 
-def is_square(x):
-    sq = round(sqrt(x))
-    if sq**2 == x:
-        return True
+def exist(x):
+    limit = int(sqrt((x-1)/2))
+    for i in range(1,limit+1):
+        if isprime(x-2*i**2):
+            return True
     return False
 
 def main():
-    odd_comp = 35
+    i = 35
     while True:
-        if not isprime(odd_comp):
-            primes = list(sieve.primerange(1,odd_comp))
-            if all(not is_square(x) for x in [(odd_comp-x)/2 for x in primes]):
-                return odd_comp
-        odd_comp += 2
+        if not isprime(i) and not exist(i):
+            return i
+        i += 2
