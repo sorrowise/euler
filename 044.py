cf1bca1d729e43c53ea61e6211371cde9c035983
metaquant <576422965@qq.com>
2019-10-17 10:17:27 +0800

    Update 044.py

--- 044.py
+++ 044.py
@@ -1,6 +1,5 @@
-# ans = 5482660, time cost = 2.34s
+# time cost = 1.55 s ± 10.6 ms
 
-from itertools import combinations
 from math import sqrt
 
 def is_pantagon(x):
@@ -8,11 +7,15 @@ def is_pantagon(x):
         return True
     return False
 
-def main(upbound=3000):
+def main():
     pantagon = lambda n : n*(3*n-1)/2
-    pantagon_list = [pantagon(n) for n in range(1,upbound)]
-    combs = list(combinations(pantagon_list,2))
-    for x in combs:
-        d = abs(x[1]-x[0])
-        if is_pantagon(sum(x)) and is_pantagon(d):
-            return d   
+    pt = [pantagon(n) for n in range(1,2000)]
+    for i in range(1,2000):
+        for j in range(0,i):
+            s1,s2 = pt[i] + 2*pt[j], 2*pt[i] + pt[j]
+            pk = pt[i] + pt[j]
+            if is_pantagon(pk):
+                if is_pantagon(s1):
+                    return pt[i]
+                if is_pantagon(s2):
+                    return pt[j]
