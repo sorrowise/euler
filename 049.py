15d9239ed2f45dd078af0fd58c3368fff0e9ed82
metaquant <576422965@qq.com>
2019-10-18 22:36:08 +0800

    Update 049.py

--- 049.py
+++ 049.py
@@ -1,11 +1,12 @@
-# ans = 296962999629, time cost = 3.18 ms
+# time cost = 3.8 ms ± 549 µs
 
-from sympy import sieve
+from sympy import isprime,primerange
 
 def main():
-    primes = list(sieve.primerange(1488,10000))
+    primes = primerange(1000,10000)
     for p in primes:
-        a = p + 3330
-        b = p + 6660
-        if a in primes and b in primes and set(str(p))==set(str(a))==set(str(b)):
-            return str(p) + str(a) + str(b)
+        if p != 1487:
+            a = p + 3330
+            b = p + 6660
+            if isprime(a) and isprime(b) and set(str(p))==set(str(a))==set(str(b)):
+                return str(p) + str(a) + str(b)
