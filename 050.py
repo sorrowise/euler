3a7f05065974579658c1d14de5e4bd182d44dc35
metaquant <576422965@qq.com>
2019-10-19 12:27:08 +0800

    Update 050.py

--- 050.py
+++ 050.py
@@ -1,5 +1,4 @@
 # time cost = 6.7 ms ± 709 µs
-
 from sympy import isprime,primerange,nextprime
 
 def primesum_below_N(N):
@@ -19,4 +18,4 @@ def main(N=10**6):
         for start in range((length-d+1)):
             res = sum(primes[start:start+d])
             if isprime(res):
-                return (primes[start],d,res)
+                return res
