84f76ee45760008dcc8b752312c64c2de9b0f6b6
metaquant <576422965@qq.com>
2019-10-20 10:36:08 +0800

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,3 +1,5 @@
-# -*- coding: utf-8 -*-
+# time cost = 123 µs ± 15.4 µs
 
-print(sum([x for x in range(1,1000) if x%3==0 or x%5==0]))
+def main():
+    ans = sum([x for x in range(1,1000) if x%3==0 or x%5==0])
+    return ans
