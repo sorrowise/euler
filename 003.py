e93ae707b0b93670290ed309cc5f78ef8656d09c
metaquant <576422965@qq.com>
2019-10-21 17:03:46 +0800

    Update 003.py

--- 003.py
+++ 003.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
+# time cost = 238 µs ± 789 ns
 
-def max_prime_factor(n):
+def main(n=600851475143):
     i = 2
     while i * i < n:
         while n%i == 0:
             n /= i
         i += 2 if i>2 else 1
     return n
-
-print max_prime_factor(600851475143)
