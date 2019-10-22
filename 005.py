6ea5f4287dd41cb6684dbc9b668bd82c315ff91d
metaquant <576422965@qq.com>
2019-10-22 11:41:28 +0800

    Update 005.py

--- 005.py
+++ 005.py
@@ -1,10 +1,25 @@
-# -*- coding: utf-8 -*-
+# approach 1, time complexity = O(n^2)
 
-from fractions import gcd
+from math import gcd
+from functools import reduce
 
-def lcm(numbers):
+def lcm(n):
     def lcm(a, b):
         return (a * b) // gcd(a, b)
-    return reduce(lcm, numbers)
+    return reduce(lcm, range(1,n+1))
 
-print lcm(range(1,21))
+# approach 2, time complexity = O(nlog(n))
+
+from sympy import primerange
+from math import sqrt,log,floor
+
+def main(n=20):
+    primes = list(primerange(1,n))
+    i,ans = 0,1
+    while primes[i] < sqrt(n):
+        e = floor(log(n)/log(primes[i]))
+        ans *= (primes[i])**e
+        i += 1
+    for p in primes[i:]:
+        ans *= p
+    return ans
