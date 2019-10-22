15a9aa97cd3ace062cee6aa9bdaabff23b007fd8
metaquant <576422965@qq.com>
2019-10-22 15:13:07 +0800

    Update 008.py

--- 008.py
+++ 008.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
+# time cost = 4.71 ms ± 3.86 µs
 
-data = ''
-f = open("euler/ep08.txt")
-for line in f.readlines():
-    data = data + line.strip()
-
-res = []
-for i in range(988):
-    sub = [int(x) for x in data[i:i+13]]
-    prod = reduce(lambda x,y:x*y, sub)
-    res.append(prod)
-
-print max(res)
+def main():
+    with open('euler/ep08.txt','r') as f:
+        data = ''
+        for line in f.readlines():
+            data = data + line.strip()
+    res = []
+    for i in range(988):
+        sub = [int(x) for x in data[i:i+13]]
+        prod = reduce(lambda x,y:x*y, sub)
+        res.append(prod)
+    return max(res)
