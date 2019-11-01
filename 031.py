9ca072e91ea3fb765b5820e989de0cd117a46c47
metaquant <576422965@qq.com>
2019-11-01 10:00:32 +0800

    Update 031.py

--- 031.py
+++ 031.py
@@ -1,12 +1,23 @@
-# time cost = 1.82 ms ± 19.9 µs
+# top-down method, time cost = 88.6 ns ± 0.468 ns
 
-def main(target=200):
-    import numpy as np
+from functools import lru_cache
+
+@lru_cache(maxsize=128)
+def ways(x,y=8):
     coins = [1, 2, 5, 10, 20, 50, 100, 200]
-    x = np.ones((target,len(coins)))
-    for i in range(1,target):
-        for j in range(1,len(coins)):
-            left = x[i][j-1]
-            up = x[i-coins[j]][j] if (i+1-coins[j])>=0 else 0
-            x[i][j] = left + up
-    return int(x[-1,-1])
+    if x == 1 or y == 1:
+        return 1
+    elif x < coins[y-1]:
+        return ways(x,y-1)
+    else:
+        return ways(x,y-1) + ways(x-coins[y-1],y)
+
+# bottom up method, time cost = 221 µs ± 686 ns
+
+def dp(target=200):
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    d = {i:1 for i in range(target+1)}
+    for j in range(1,len(coins)):
+        for i in range(coins[j],target+1):
+            d[i] = d[i] + d[i-coins[j]]
+    return d[target] 
