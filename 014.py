1d994bf70afd5a33ef2f26027f686c9c69a42451
metaquant <576422965@qq.com>
2019-11-01 21:53:24 +0800

    Update 014.py

--- 014.py
+++ 014.py
@@ -1,18 +1,19 @@
-# -*- coding: utf-8 -*-
+# time cost = 1.26 s ± 105 ms
 
-import numpy as np
-
-def num_of_terms(n):
-    collatz = lambda n : 3*n+1 if n%2==1 else n//2
-    num = 1
-    while n !=1:
-        n = collatz(n)
-        num += 1
-    return num
-
-def max_start(n):
-    terms_arr = np.array([num_of_terms(x) for x in range(1,n+1)])
-    index = terms_arr.argmax() + 1
-    return index
-
-print max_start(1000000)
+def main(N=10**6):
+    d = {}
+    for x in range(2,N):
+        i = x
+        n = 0
+        while x != 1:
+            if x < i:
+                n = n + d[x]
+                break
+            elif x % 2 == 0:
+                x = x // 2
+                n += 1
+            else:
+                x = (3*x+1) // 2
+                n += 2
+        d[i] = n
+    return max(d,key=d.get)
