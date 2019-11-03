d55c3d7cea9e241ed714031000e0b46d1ac598f6
metaquant <576422965@qq.com>
2019-11-03 11:12:12 +0800

    Update 015.py

--- 015.py
+++ 015.py
@@ -1,11 +1,21 @@
-# -*- coding: utf-8 -*-
+# approach 1, time cost = 140 ns ± 11.8 ns
+
+from functools import lru_cache
+
+@lru_cache(maxsize=128)
+def path(x,y):
+    if x == 0 or y == 0:
+        return 1
+    else:
+        return path(x-1,y) + path(x,y-1)
+
+# approach 2, time cost = 1.76 µs ± 83.9 ns
+
+from math import factorial as fac
 
 def comb_num(n,k):
-    fac = lambda n:reduce(lambda x,y:x*y,range(1,n+1))
-    num = fac(n)/(fac(n-k)*fac(k))
+    num = fac(n)//(fac(n-k)*fac(k))
     return num
 
-def lattice_paths(n,k):
+def main(n,k):
     return comb_num(n+k,k)
-
-print lattice_paths(20,20)
