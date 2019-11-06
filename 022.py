66eacf8da870a49bd22182373b06530acba119c6
metaquant <576422965@qq.com>
2019-11-06 14:54:05 +0800

    Update 022.py

--- 022.py
+++ 022.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
-
-with open('euler/ep22.txt','r') as f:
-    data = f.read().replace('"',"").split(',')
+# time cost = 246 ms ± 608 µs
 
 def alphabet(word):
     alpha = lambda s : ord(s) - ord('A') + 1
     res = sum([alpha(x) for x in word])
     return res
 
-def sum_name_score(data):
-    name_score = lambda word : alphabet(word) * (sorted(data).index(word)+1)
-    res = sum([name_score(word) for word in data])
-    return res
+def main():
+    with open('data/ep22.txt','r') as f:
+        names = f.read().replace('"',"").split(',')
+    sorted_names = sorted(names)
+    name_score = lambda word : alphabet(word) * (sorted_names.index(word)+1)
+    ans = sum([name_score(word) for word in names])
+    return ans
