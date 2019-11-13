ef1163c7a0d42ac7ad7b68423cc006f235a10023
metaquant <576422965@qq.com>
2019-11-13 15:37:57 +0800

    Update 056.py

--- 056.py
+++ 056.py
@@ -1,7 +1,6 @@
-# ans = 972, time cost = 190ms
+# time cost = 190 ms ± 515 µs
 
 def main():
     str_sum = lambda y : sum([int(x) for x in str(y)])
     res = max([str_sum(a**b) for a in range(1,100) for b in range(1,100)])
     return res
-
