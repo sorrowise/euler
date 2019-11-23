b7a2bebe4a55cb7b09c496bbe4bbd70f3ab6ac70
metaquant <576422965@qq.com>
2019-11-23 16:49:12 +0800

    Update 063.py

--- 063.py
+++ 063.py
@@ -1,9 +1,9 @@
-# ans = 49, time cost = 168 µs
+# time cost = 2.8 µs ± 29.8 ns
 
-def diff(x):
-    d = [n-len(str(x**n)) for n in range(1,25)]
-    res = [x for x in d if x==0]
-    return len(res)
+from math import log10
 
 def main():
-    return sum([diff(x) for x in range(1,10)])
+    c = 0
+    for n in range(1,10):
+        c += int(1/(1-log10(n)))
+    return c
