ec92eb5d56d553d5a858eec2e201ed920b0ceb3a
metaquant <576422965@qq.com>
2019-11-23 15:43:28 +0800

    Update 062.py

--- 062.py
+++ 062.py
@@ -1,14 +1,15 @@
-# time cost = 2.27 s ± 11.3 ms
+# time cost = 1.65 s ± 7.11 ms
 
-from collections import Counter
 from itertools import count
+
+perm = lambda x : "".join(sorted(str(x)))
 
 def cubic_perm(res):
     for i in count(5,1):
-        c = Counter(str(i**3))
+        c = perm(i**3)
         if len([x for x in res if x==c]) == 5:
             return i**3
 
 def main():
-    res = [Counter(str(i**3)) for i in range(5,10001)]
+    res = [perm(i**3) for i in range(5,10001)]
     return cubic_perm(res)
