4302e7b20b7ea3e0e47654aad17b0f33b581b4f1
metaquant <576422965@qq.com>
2019-11-29 15:05:01 +0800

    Update 068.py

--- 068.py
+++ 068.py
@@ -1,2 +1,2 @@
-# No programming is required to solve this problem
+# this problem can be solved with pen and paper, no programming is required.
 # see https://www.cnblogs.com/metaquant/p/11957802.html for an explanation
