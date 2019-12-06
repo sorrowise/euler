3c79c6f79acf33c2f47d80ee748e8aefdda20bbb
metaquant <576422965@qq.com>
2019-12-06 15:28:08 +0800

    Update 072.py

--- 072.py
+++ 072.py
@@ -2,12 +2,20 @@
 
 from functools import lru_cache
 
+def number_theory_block(f,n,i=1):
+    ans = 0
+    while i <= n:
+        j = n//(n//i)
+        ans += (j-i+1)*f(n//i)
+        i = j + 1
+    return ans
+
 @lru_cache(maxsize=2048)
 def sum_of_euler_phi(n):
     if n == 1:
         return 1
     else:
-        return n*(n+1)//2 - sum([sum_of_euler_phi(n//i) for i in range(2,n+1)])
+        return n*(n+1)//2 - number_theory_block(sum_of_euler_phi,n,2)
 
 def main(N=10**6):
     return sum_of_euler_phi(N)-1
