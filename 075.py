dfcfef0e0e7d5ca9e7563b280457e4b11372240c
metaquant <576422965@qq.com>
2019-12-11 21:16:28 +0800

    Update 075.py

--- 075.py
+++ 075.py
@@ -1,17 +1,16 @@
-# time cost = 823 ms ± 3.39 ms
+# time cost = 1.37 s ± 63.6 ms
 
 from math import gcd
+from collections import Counter
 
 def main(N=1500000):
-    dt,limit = {},int((N//2)**0.5)+1
+    limit,arr = int((N//2)**0.5)+1,[]
     for m in range(2,limit):
         for n in range(1,m):
             if (m+n)%2 == 1 and gcd(m,n) == 1:
                 p,k = 2*m*(m+n),1
                 while k*p <= N:
-                    if k*p in dt:
-                        dt[k*p] += 1
-                    else:
-                        dt[k*p] = 1
+                    arr.append(k*p)
                     k += 1
-    return len([x for x in dt.values() if x==1])
+    c = Counter(arr)
+    return len([x for x in c.values() if x==1])
