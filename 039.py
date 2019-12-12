0972e7c3f245d98b4a652d842f78d27d3ba4b657
metaquant <576422965@qq.com>
2019-12-12 08:52:44 +0800

    Update 039.py

--- 039.py
+++ 039.py
@@ -1,4 +1,4 @@
-# time cost = 60.1 ms ± 626 µs
+# approach1, time cost = 60.1 ms ± 626 µs
 
 def number_of_solution(p):
     num = 0
@@ -11,3 +11,20 @@ def main(n=1000):
     d = {p:number_of_solution(p) for p in range(2,n+1,2)}
     ans = max(d,key=d.get)
     return ans
+
+# approach 2, time cost = 192 µs ± 844 ns
+
+from math import gcd
+from collections import Counter
+
+def max_number_of_solution(N=1000):
+    limit,arr = int((N//2)**0.5)+1,[]
+    for m in range(2,limit):
+        for n in range(1,m):
+            if (m+n)%2 == 1 and gcd(m,n) == 1:
+                p,k = 2*m*(m+n),1
+                while k*p <= N:
+                    arr.append(k*p)
+                    k += 1
+    c = Counter(arr)
+    return c.most_common()[0][0]
