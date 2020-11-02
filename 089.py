002e3ea129e94d04f00207be4a9c4fcc005871ca
metaquant <576422965@qq.com>
2020-11-02 17:21:17 +0800

    Create 089.py

---
+++ 089.py
@@ -0,0 +1,8 @@
+# time cost = 565 µs ± 11.4 µs
+
+def main():
+    with open('data/ep89.txt') as f:
+        romans = f.read()
+    new = re.sub("DCCCC|LXXXX|VIIII|CCCC|XXXX|IIII", '**',romans)
+    savings = len(romans) - len(new)
+    return savings
