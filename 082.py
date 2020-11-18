44851255fcc53602c814e3b51770d075ee8579d0
metaquant <576422965@qq.com>
2020-11-18 11:44:07 +0800

    Update 082.py

--- 082.py
+++ 082.py
@@ -1,19 +1,17 @@
-# time cost = 15.3 ms ± 102 µs
+# time cost = 7.4 ms ± 81.7 µs
 
-import numpy as np
-
-def minimal_path_sum(matrix):
+def min_path_sum(matrix):
     n = len(matrix)
-    mat = np.array(matrix)
-    cost = mat.cumsum(axis=1)
-    for j in range(1,n):
-        for i in range(1,n):
-                cost[i,j] = mat[i,j] + min(cost[i,j-1],cost[i-1,j])
-        for i in range(n-2,-1,-1):
-                cost[i,j] = min(cost[i,j],cost[i+1,j]+mat[i,j])
-    return min(cost[:,-1])
+    cost = [matrix[i][0] for i in range(n)]
+    for column in range(1,n):
+        cost[0] += matrix[0][column]
+        for row in range(1,n):
+            cost[row] = min(cost[row], cost[row-1]) + matrix[row][column]
+        for row in range(n-2, -1, -1):
+            cost[row] = min(cost[row], cost[row+1] + matrix[row][column])
+    return min(cost)
 
 def main():
     with open('data/pe082.txt') as f:
         matrix = [[int(x) for x in row.split(',')] for row in f.readlines()]
-    return minimal_path_sum(matrix)
+    return min_path_sum(matrix)
