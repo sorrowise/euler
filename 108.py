e10ed154b116e490bb6410846d36fbad995ea4ec
metaquant <576422965@qq.com>
2021-01-06 15:05:14 +0800

    Update 108.py

--- 108.py
+++ 108.py
@@ -11,7 +11,7 @@ def prime_product(factors):
     res = reduce(mul,[b**p for b,p in zip(bases,powers)])
     return res
 
-def main(N=4*10**6):
+def main(N=1000):
     n = 2*N - 1
     primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
     while True:
