8a73608b379ad7a31369304e6d4bc7e5393757ee
metaquant <576422965@qq.com>
2021-02-09 17:13:11 +0800

    Update 116.py

--- 116.py
+++ 116.py
@@ -1,4 +1,18 @@
-# time cost = 426 µs ± 3.03 µs
+# approach 1, time cost = 583 ns ± 2.65 ns
+
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def f(n,m):
+    if n < m:
+        return 1
+    else:
+        return f(n-m,m)+f(n-1,m)
+
+def main(n=50):
+    return f(n,2)+f(n,3)+f(n,4)-3
+
+# approach 2, time cost = 426 µs ± 3.03 µs
 
 from scipy.special import comb
 
