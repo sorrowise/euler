2e6d6d475cc9a36b01a84fbd81d24c45581e5e8a
metaquant <576422965@qq.com>
2021-03-04 09:23:27 +0800

    Update 121.py

--- 121.py
+++ 121.py
@@ -7,5 +7,5 @@ def main(n=15):
     x = sp.var('x')
     expr = prod([(x+i) for i in range(1,n+1)])
     p = sp.Poly(expr)
-    res = factorial(16)//sum(p.coeffs()[:n//2+1])
+    res = factorial(n+1)//sum(p.coeffs()[:n//2+1])
     return res
