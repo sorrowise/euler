f702b41c1eb45746bd268ce21ddc8e5d2fee7e90
metaquant <576422965@qq.com>
2021-03-06 16:39:12 +0800

    Update 243.py

--- 243.py
+++ 243.py
@@ -1,20 +1,16 @@
-# time cost = 53.2 ms ± 517 µs
+# time cost = 49.4 ms ± 395 µs
 
-from sympy import prime,primorial
+from sympy import prime
 from fractions import Fraction as f
 from math import prod
 
-def resilience(n,k=1):
-    primes = [prime(i) for i in range(1,n+1)]
-    prod_p = prod(primes)
-    prod_q = prod([x-1 for x in primes])
-    res = f(k*prod_p,k*prod_p-1)*f(prod_q,prod_p)
-    return res
-
-def main(target=f(15499,94744)):
-    n,k = 1,2
-    while resilience(n) > target:
+def main(a=15499,b=94744):
+    fy = lambda n:prod([prime(i) for i in range(1,n+1)])
+    fx = lambda n:prod([prime(i)-1 for i in range(1,n+1)])
+    n = 1
+    while True:
+        if f(fy(n),fx(n)) > f(b,a):
+            break
         n = n + 1
-    while resilience(n-1,k) > target:
-        k = k + 1
-    return k*primorial(n-1)
+    k = int(a/(a*fy(n)-b*fx(n)))+1
+    return k * fy(n)
