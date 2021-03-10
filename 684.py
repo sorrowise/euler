352ae615e843f6411ba637a48ef1eb6ec84e89e6
metaquant <576422965@qq.com>
2021-03-10 11:33:10 +0800

    Create 684.py

---
+++ 684.py
@@ -0,0 +1,12 @@
+# time cost = 271 µs ± 3.42 µs
+
+def s(k,r,m=10**9+7):
+    res = ((r+2)*(r+1)//2+5)*pow(10,k,m)-9*k-6-r
+    return res
+
+def main(n=90):
+    fib = [0,1]
+    for _ in range(n-1):
+        fib.append(sum(fib[-2:]))
+    res = [s(*divmod(fib[i],9)) for i in range(2,n+1)]
+    return sum(res)%(10**9+7)
