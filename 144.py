1af1e704f78dc61c1973a3d3730f30da619de921
metaquant <576422965@qq.com>
2021-04-22 10:00:52 +0800

    Update 144.py

--- 144.py
+++ 144.py
@@ -1,21 +1,11 @@
-# time cost = 791 µs ± 3.81 µs 
-
-def quad_solver(a,b,c):
-    delta = b**2-4*a*c
-    x1 = (-b+delta**0.5)/(2*a)
-    x2 = (-b-delta**0.5)/(2*a)
-    return x1,x2
+# time cost = 420 µs ± 322 ns
 
 def next_point(x0,y0,x1,y1):
     i = (y1-y0)/(x1-x0)
     n = y1/(4*x1)
     r = (-i+2*n+i*n**2)/(1+2*i*n-n**2)
     m = y1-r*x1
-    x21,x22 = quad_solver(4+r**2,2*m*r,m**2-100)
-    if abs(x21-x1)>abs(x22-x1):
-        x_new = x21
-    else:
-        x_new = x22
+    x_new = (-2*r*m)/(4+r**2)-x1
     y_new = r * x_new + m
     return x_new,y_new
 
