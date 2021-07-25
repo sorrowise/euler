aca897b4118432ef3a997afa45889a7000b41b2c
metaquant <576422965@qq.com>
2021-07-25 19:58:21 +0800

    Create 204.py

---
+++ 204.py
@@ -0,0 +1,18 @@
+# time cost = 103 ns ± 3.11 ns
+
+from math import log
+from sympy import isprime
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def hn(n,f):
+    if n == 1:
+        return 1
+    elif f == 2:
+        return int(log(n,2))+1
+    elif n < f:
+        return hn(n,n)
+    elif not isprime(f):
+        return hn(n,f-1)
+    else:
+        return hn(n,f-1)+hn(n//f,f)
