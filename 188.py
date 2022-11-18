02adc5454c787129b5433b1503d1d9d0cf985ab0
metaquant <576422965@qq.com>
2022-11-18 18:14:28 +0800

    Update 188.py

--- 188.py
+++ 188.py
@@ -5,3 +5,14 @@ def main(b=1777,e=1855,m=10**8):
     for _ in range(e):
         res = pow(b,res,m)
     return res
+
+
+# time cost = 4.74 ms ± 9.72 µs
+
+from sympy.ntheory import totient
+
+def main(b=1777,e=1855,m=10**8):
+    res = b
+    for _ in range(e):
+        res = pow(b,res%totient(m),m)
+    return res
